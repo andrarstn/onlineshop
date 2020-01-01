01c9b5e2be4eb7aaf15b0410e3fd84b9c5fc7547
Andra Ristiano <56077597+andrarstn@users.noreply.github.com>
2020-01-01 08:41:43 +0700

    Add files via upload
    Add new scripts

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,29 @@
-const singleproduct = document.querySelector('.singleproduct');
+const singleProduct = document.querySelector('#singleproduct');
 const thumb = document.querySelectorAll('.prod-thumb');
 const figImg = document.querySelector('.figure-img');
 
-singleproduct.addEventListener('click', function (e) {
+const buttonMin = document.querySelector('.min');
+const buttonPlus = document.querySelector('.plus');
+const quantity = document.querySelector('.quantity');
+let quantityProduct = document.querySelector('.quantity span');
+let numb = 1;
+buttonPlus.addEventListener('click', function () {
+    numb++;
+    quantityProduct.innerHTML = numb;
+})
+buttonMin.addEventListener('click', function () {
+    numb--;
+    quantityProduct.innerHTML = numb;
+    if (numb == 0) {
+        const newAlert = document.createElement('div');
+        const alert = document.createTextNode('Angka harus lebih dari 0');
+        newAlert.appendChild(alert);
+        newAlert.setAttribute('class', 'alert alert-warning');
+        newAlert.setAttribute('role', 'alert');
+        quantity.insertBefore(newAlert, buttonMin);
+    }
+})
+singleProduct.addEventListener('click', function (e) {
     if (e.target.className == 'prod-thumb') {
         thumb.forEach(function (t) {
             if (t.classList.contains('active-thumb')) {
